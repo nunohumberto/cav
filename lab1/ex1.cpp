937c18b9f6ab88a94057079fdb59e92e44ca301a
Nuno Paula <nunohumberto@ua.pt>
2017-10-16 06:20:27 +0100

    Text generation now works. FCM class created. Input text and output text now work with files. Symbol statistics methods created.

--- lab1/ex1.cpp
+++ lab1/ex1.cpp
@@ -3,53 +3,50 @@
 #include <math.h>
 #include <map>
 #include <algorithm>
+#include <fstream>
 
 #define CONTEXT_SIZE 1
-#define RESULT_SIZE 50
-#define ORDER 1
+//#define RESULT_SIZE 100
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+class FCM {
+        string dataset;
+        int ORDER;
+        map<string, map<char,int> > setmap;
+    public:
+        FCM(int);
+        void addToDataset(char);
+        void addToDataset(string);
+        void fillInternalMap();
+        string generateText(int);
+};
 
-    if (argc != 2) {
-        cout << "No data input.\n";
-        return 1;
-    }
+FCM::FCM(int order) {
+    ORDER = order;
+}
 
-    string dataset(argv[1]);
-    map<string, map<char,int> > setmap;
+void FCM::addToDataset(char c) {
+    dataset = dataset + c;
+};
 
+void FCM::addToDataset(string c) {
+    dataset = dataset + c;
+};
+
+void FCM::fillInternalMap() {
     for(int i = 0; i < dataset.size()-ORDER; i++) {
-        string s(1, dataset[i]);
+        string s(dataset, i, ORDER);
         //string s = "a";
         if(setmap.count(s) == 0){
             setmap[s] = map<char,int>();
             setmap[s][dataset[i+ORDER]] = 0;
         }
         setmap[s][dataset[i+ORDER]]++;
-        //cout << "Added the key to " << s << endl;
     }
+}
 
-
-    /*
-    double state_sum;
-    double entropy, state_entropy, prob;
-    int total = dataset.size() - CONTEXT_SIZE;
-    for(map<string, map<char,int> >::iterator it = setmap.begin(); it != setmap.end(); ++it) {
-        state_sum = 0;
-        state_entropy = 0;
-        for(map<char,int>::iterator inner = setmap[it->first].begin(); inner != setmap[it->first].end(); ++inner) {
-            state_sum += setmap[it->first][inner->first];
-        }
-
-        for(map<char,int>::iterator inner = setmap[it->first].begin(); inner != setmap[it->first].end(); ++inner) {
-            prob = setmap[it->first][inner->first]/state_sum;
-            state_entropy += -prob * log2(prob);
-        }
-
-        entropy += state_entropy * (state_sum/total);
-    }*/
+string FCM::generateText(int length) {
 
     string result;
 
@@ -59,13 +56,15 @@ int main(int argc, char* argv[]) {
     int maxtimes=0;
 
     map<string, map<char,int> >::iterator it;
-    for(it = setmap.begin(); it != setmap.end(); ++it) {
+    for (it = setmap.begin(); it != setmap.end(); ++it) {
+        //cerr << "Trying " << it->first << endl;
         int num = 0;
-        for(map<char,int>::iterator inner = setmap[it->first].begin(); inner != setmap[it->first].end(); ++inner) {
+        for (map<char, int>::iterator inner = setmap[it->first].begin();
+             inner != setmap[it->first].end(); ++inner) {
             num += setmap[it->first][inner->first];
         }
 
-        if( num > maxtimes){
+        if (num > maxtimes) {
             maxtimes = num;
             c = it->first;
         }
@@ -73,24 +72,40 @@ int main(int argc, char* argv[]) {
 
     result = result + c;
 
+    //cerr << "Preliminary result: " << result << "\n";
+
+
     // write the rest of generated text with given context
     char b; //char to be added
 
-    while(result.length() < RESULT_SIZE){
+    while(result.length() < length){
 
         map <char, double> textPercent; //percentage of char in generated text
+        map<string, map<char,int> > result_setmap;
         bool wfound = false; //context found
 
 
         string w = result.substr(result.length()-ORDER,result.length()); //context
-        cout << "Context " << w << endl;
+        //cout << "Context: '" << w << '\'' << endl;
+
+
+
+        for(int i = 0; i < result.size()-ORDER; i++) {
+            string s(result, i, ORDER);
+            //string s = "a";
+            if(result_setmap.count(s) == 0){
+                result_setmap[s] = map<char,int>();
+                result_setmap[s][result[i+ORDER]] = 0;
+            }
+            result_setmap[s][result[i+ORDER]]++;
+        }
 
 
         for(int i = 0; i < dataset.size()-ORDER; i++) {
             if(textPercent.count(dataset[i]) == 0){
-                size_t n = std::count(result.begin(), result.end(), dataset[i]);
+                long n = std::count(result.begin(), result.end(), dataset[i]);
                 textPercent[dataset[i]] = ((double) n)/result.size();
-                cout << "\t" << dataset[i] << " -> " << textPercent[dataset[i]] << endl;
+                //cout << "\t" << dataset[i] << " -> " << textPercent[dataset[i]] << endl;
             }
         }
 
@@ -102,34 +117,93 @@ int main(int argc, char* argv[]) {
 
         for (map<char, int>::iterator inner = setmap[w].begin(); inner != setmap[w].end(); ++inner) {
             int bnum = setmap[w][inner->first];
-            if (bnum > maxbnum && ((double) bnum)/dataset.size() >= textPercent[inner->first]) {
+            double percent_in_result = -1;
+            bool only_one_possibility = false;
+            if (result_setmap.count(w) > 0 && result_setmap[w].count(inner->first) > 0) {
+                percent_in_result = ((double) result_setmap[w][inner->first]) / result.size();
+            }
+            if (setmap.count(w) > 0 && setmap[w].size() == 1) {
+                only_one_possibility = true;
+            }
+            //cout << "Checking for '" << inner->first << "'\n";
+            if (bnum > maxbnum  && ( only_one_possibility || percent_in_result == -1 || ((double) bnum)/dataset.size() >= percent_in_result )) {
+                //cout << "Since bnum (" << bnum << ") > maxbnum(" << maxbnum << ") <-- Condition 1 OK!\n";
+                //cout << "Since the prob (" << ((double) bnum)/dataset.size() << ") >= textpercent(" << textPercent[inner->first] << ") <-- Condition 2 OK!\n";
+                //cout << "Since the prob (" << ((double) bnum)/dataset.size() << ") >= textpercent(" << percent_in_result << ") OR only one possibility: " << only_one_possibility << " <-- Condition 2 OK!\n";
+
                 maxbnum = bnum;
                 b = inner->first;
                 wfound = true;
+            } else {
+                //cout << "Since bnum (" << bnum << ") > maxbnum(" << maxbnum << ") <-- Condition 1 may not be OK!\n";
+                //cout << "Since the prob (" << ((double) bnum)/dataset.size() << ") >= textpercent(" << textPercent[inner->first] << ") <-- Condition 2 may not be OK!\n";
+                //cout << "Since the prob (" << ((double) bnum)/dataset.size() << ") >= textpercent(" << percent_in_result << ") OR only one possibility: " << only_one_possibility << " <-- Condition 2 may not be OK!\n";
+
             }
         }
 
-        cout << "wfound = " << wfound << '\n';
+        //cout << "wfound = " << wfound << '\n';
         if(!wfound){ //if context not found
-            int maxnum = 0;
+            long maxnum = 0;
             int i;
             for(i = 0; i < dataset.length(); ++i){
-                size_t ndataset = std::count(dataset.begin(), dataset.end(), dataset[i]);
-                double datasetpercent = ndataset/dataset.length();
-                if(datasetpercent <= textPercent[dataset[i]] && maxnum < ndataset){
+                long ndataset = std::count(dataset.begin(), dataset.end(), dataset[i]);
+
+                double datasetpercent = ((double) ndataset)/dataset.length();
+
+                //cout << "New candidate: " << dataset[i] << " with ndataset " << ndataset << " and percent " << datasetpercent << ", textpercent " << textPercent[dataset[i]]<< endl;
+                if(datasetpercent >= textPercent[dataset[i]] && maxnum < ndataset){
                     maxnum = ndataset;
                     b = dataset[i];
                 }
             }
         }
 
-        cout << "Added " << b << endl;
+        //cout << "Added " << b << endl;
         result = result + b;
 
-        cout << "Result: '" << result << '\'' << endl;
+        //cout << "Result: '" << result << '\'' << endl;
+    }
+    return result;
+
+
+}
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 4) {
+        cout << "Usage: prog <file name> <model order> <desired generated text length>\n";
+        return 1;
     }
 
-    cout << result << endl;
+    string buffer;
+    string dataset;
+    ifstream input (argv[1]);
+    if (input.is_open())
+    {
+        while (getline(input, buffer))
+        {
+            dataset = dataset + buffer;
+        }
+        input.close();
+    }
+
+    int order = stoi(argv[2]);
+    int len = stoi(argv[3]);
+
+
+    FCM fcm(order);
+
+    fcm.addToDataset(dataset);
+    fcm.fillInternalMap();
+
+    ofstream output;
+    string outfilename("output.txt");
+    output.open (outfilename);
+    output << fcm.generateText(len);
+    output.close();
+
+    cout << "Generated text printed to " << outfilename << endl;
 
     return 0;
 
